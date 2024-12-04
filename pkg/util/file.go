b65a3aab4fddf6d374b9f7ae7c4d7c495047a212
Rafael da Fonseca <rafael.fonseca@wildlifestudios.com>
2024-12-04 12:03:24 +0000

    Re-write uncommenter

--- pkg/util/file.go
+++ pkg/util/file.go
@@ -3,6 +3,7 @@ package util
 import (
 	"bufio"
 	"errors"
+	"fmt"
 	"io"
 	"os"
 	"path"
@@ -64,17 +65,16 @@ func PrefixFirstYamlDocument(line, file string) error {
 // RemoveCommentsFromYaml tries to remove comments if they contain valid yaml
 func RemoveCommentsFromYaml(reader io.Reader) ([]byte, error) {
 	result := make([]byte, 0)
-	buff := make([]byte, 0)
 	scanner := bufio.NewScanner(reader)
 
 	helmDocsMatcher := regexp.MustCompile(`^\s*#\s*--`)
-	commentMatcher := regexp.MustCompile(`^\s*#\s*`)
-	commentYamlMapMatcher := regexp.MustCompile(`^(\s*#\s*)[^:]+:.*$`)
+	commentMatcher := regexp.MustCompile(`^(\s*#\s*)(.*$)`)
+	commentYamlMapMatcher := regexp.MustCompile(`^(\s*#\s*)([^:]+:)(.*$)`)
+	whitespaceMatcher := regexp.MustCompile(`\s`)
 	schemaMatcher := regexp.MustCompile(`^\s*#\s@schema\s*`)
 
 	var line string
-	var inCode, inSchema bool
-	var codeIndention int
+	var inDocs, inSchema bool
 	var unknownYaml interface{}
 	var headerCommentsParsed bool
 
@@ -87,27 +87,15 @@ func RemoveCommentsFromYaml(reader io.Reader) ([]byte, error) {
 				appendAndNLStr(&result, line)
 				continue
 			} else {
-				appendAndNLStr(&result, "")
 				headerCommentsParsed = true
 			}
 
 		}
 
 		// Don't try to uncomment helm-docs descriptions
-		if helmDocsMatcher.Match([]byte(line)) && !inCode {
+		if helmDocsMatcher.Match([]byte(line)) {
+			inDocs = true
 			appendAndNLStr(&result, line)
-			continue
-		}
-
-		// If the line is empty and we are parsing a block of potential yaml,
-		// the parsed block of yaml is "finished" and should be added to the
-		// result
-		if line == "" && inCode {
-			appendAndNL(&result, &buff)
-			appendAndNLStr(&result, line)
-			// reset
-			inCode = false
-			buff = make([]byte, 0)
 			continue
 		}
 
@@ -119,50 +107,61 @@ func RemoveCommentsFromYaml(reader io.Reader) ([]byte, error) {
 			continue
 		}
 
-		// Inside a @schema block
+		// Inside a @schema
 		if inSchema {
+
 			appendAndNLStr(&result, line)
 			continue
 		}
 
-		// Havent found a potential yaml block yet
-		if !inCode {
-			if matches := commentYamlMapMatcher.FindStringSubmatch(line); matches != nil {
-				codeIndention = len(matches[1])
-				inCode = true
+		matches := commentYamlMapMatcher.FindStringSubmatch(line)
+		// Inside helm-docs block
+		if inDocs {
+			if len(matches) == 0 {
+				appendAndNLStr(&result, line)
+				continue
+			} else {
+				// check for the potential case of having <text>: inside helm-docs comment, this will only fail if the comment line actually starts with <text>:
+				if whitespaceMatcher.MatchString(matches[2]) {
+					appendAndNLStr(&result, line)
+					continue
+				}
+				inDocs = false
 			}
+
 		}
 
-		// Try if this line is valid yaml
-		if inCode {
-			if commentMatcher.Match([]byte(line)) {
-				// Strip the comment away
-				strippedLine := line[codeIndention:]
-				// add it to the already parsed valid yaml
-				appendAndNLStr(&buff, strippedLine)
-				// check if the new block is still valid yaml
-				err := yaml.Unmarshal(buff, &unknownYaml)
-				if err != nil {
-					// Invalid yaml found,
-					// Remove the stripped line again
-					buff = buff[:len(buff)-len(strippedLine)-1]
-					// and add the commented line instead
-					appendAndNLStr(&buff, line)
-				}
-				// its still valid yaml
-				continue
+		// If this matches a potential commented yaml
+		matches = commentMatcher.FindStringSubmatch(line)
+		if len(matches) > 0 {
+			// Strip the comment away
+
+			cleanWhitespace := strings.Replace(matches[1], "#", "", 1)
+			whitespaceSize := len(cleanWhitespace)
+			// Check if the number of spaces is even, if it's not it's likely someone added an extra whitespace along with the #
+			if whitespaceSize > 0 && whitespaceSize&1 == 1 {
+				cleanWhitespace = cleanWhitespace[1:]
 			}
+			// join the whitespace with the data
+			strippedLine := cleanWhitespace + matches[2]
+
+			// add it to the already parsed valid yaml
+			appendAndNLStr(&result, strippedLine)
 
 			// If the line is not a comment it must be yaml
-			appendAndNLStr(&buff, line)
-			continue
+			//appendAndNLStr(&buff, line)
+			//continue
+		} else {
+			// line is not a commented yaml
+			appendAndNLStr(&result, line)
 		}
-		// line is valid yaml
-		appendAndNLStr(&result, line)
 	}
-
-	if len(buff) > 0 {
-		appendAndNL(&result, &buff)
+	// check if the new block is still valid yaml
+	err := yaml.Unmarshal(result, &unknownYaml)
+	if err != nil {
+		// Invalid yaml found,
+		fmt.Println(err)
+		panic("Invalid yaml after uncommenting:\n" + string(result))
 	}
 
 	return result, nil
